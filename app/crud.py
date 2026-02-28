5baf0d33eaad750bb072bfc4e4c43af29656598c
MariaVilma12 <vilmashekar26@gmail.com>
2026-02-28 12:35:01 +0100

    rest api contract and impl inital commit

---
+++ app/crud.py
@@ -0,0 +1,23 @@
+from app import models
+from sqlalchemy.orm import Session
+
+def get_tasks(db: Session):
+    return db.query(models.Task).all()
+
+def get_task(db: Session, task_id: int):
+    return db.query(models.Task).filter(models.Task.id == task_id).first()
+
+def create_task(db: Session, task_data: dict):
+    db_task = models.Task(**task_data)
+    db.add(db_task)
+    db.commit()
+    db.refresh(db_task)
+    return db_task
+
+def delete_task(db: Session, task_id: int):
+    task = get_task(db, task_id)
+    if not task:
+        return None
+    db.delete(task)
+    db.commit()
+    return task
